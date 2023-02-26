28f3e8b87d30043821cacdbf4facce700f500e7d
Luigi <119954243+clarintux@users.noreply.github.com>
2023-02-26 13:05:20 +0000

    Update my guix configs.

--- config.scm
+++ config.scm
@@ -1,22 +1,25 @@
-;; This is an operating system configuration generated
-;; by the graphical installer.
-;;
-;; Once installation is complete, you can learn and modify
-;; this file to tweak the system configuration, and pass it
-;; to the 'guix system reconfigure' command to effect your
-;; changes.
-
-
 ;; Indicate which modules to import to access the variables
 ;; used in this configuration.
 (use-modules (gnu))
-(use-service-modules cups desktop networking ssh xorg)
+(use-service-modules cups desktop networking xorg sysctl)
 
 (operating-system
   (locale "it_IT.utf8")
   (timezone "Europe/Berlin")
   (keyboard-layout (keyboard-layout "de"))
-  (host-name "laptop")
+  (host-name "guix")
+  (issue "Benvenuto! Questo e' il tuo sistema operativo!!!\n\n")
+  (sudoers-file
+    (plain-file "sudoers" "\
+root ALL=(ALL) ALL
+%wheel ALL=(ALL) ALL
+"))
+  (hosts-file
+                  (plain-file "hosts" "\
+127.0.0.1       localhost       guix
+::1             localhost       guix
+# Ads, tracking and general junk
+"))
 
   ;; The list of user accounts ('root' is implicit).
   (users (cons* (user-account
@@ -27,43 +30,126 @@
                   (supplementary-groups '("wheel" "netdev" "audio" "video")))
                 %base-user-accounts))
 
-  ;; Packages installed system-wide.  Users can also install packages
-  ;; under their own account: use 'guix search KEYWORD' to search
-  ;; for packages and 'guix install PACKAGE' to install a package.
-  (packages (append (list (specification->package "nss-certs"))
+  ;; Packages installed system-wide.
+  (packages (append (list (specification->package "bspwm")
+                          (specification->package "sxhkd")
+                          (specification->package "tint2")
+                          (specification->package "nitrogen")
+                          (specification->package "dmenu")
+                          (specification->package "dunst")
+                          (specification->package "picom")
+                          (specification->package "lxappearance")
+                          (specification->package "mousepad")
+                          (specification->package "volumeicon")
+                          (specification->package "font-dejavu")
+                          (specification->package "font-bitstream-vera")
+                          (specification->package "font-adobe-source-code-pro")
+                          (specification->package "font-google-noto")
+                          (specification->package "font-google-noto-emoji")
+                          (specification->package "font-awesome")
+                          (specification->package "icecat")
+                          (specification->package "xterm")
+                          (specification->package "alacritty")
+                          (specification->package "zsh")
+                          (specification->package "zsh-autosuggestions")
+                          (specification->package "zsh-syntax-highlighting")
+                          (specification->package "vim")
+                          (specification->package "vim-airline-themes")
+                          (specification->package "neofetch")
+                          (specification->package "htop")
+                          (specification->package "openssh")
+                          (specification->package "curl")
+                          (specification->package "git")
+                          (specification->package "xrdb")
+                          (specification->package "thunar")
+                          (specification->package "ranger")
+                          (specification->package "neomutt")
+                          (specification->package "newsboat")
+                          (specification->package "zathura")
+                          (specification->package "sxiv")
+                          (specification->package "mpv")
+                          (specification->package "scrot")
+                          (specification->package "mpd")
+                          (specification->package "mpc")
+                          (specification->package "ncmpcpp")
+                          (specification->package "iptables")
+                          (specification->package "dnscrypt-proxy")
+                          (specification->package "nss-certs"))
                     %base-packages))
 
   ;; Below is the list of system services.  To search for available
   ;; services, run 'guix system search KEYWORD' in a terminal.
   (services
-   (append (list (service network-manager-service-type)
-                 (service wpa-supplicant-service-type)
-                 (service ntp-service-type)
-                 (service cups-service-type))
+    (cons* (service cups-service-type)
+           (service iptables-service-type
+             (iptables-configuration
+               (ipv4-rules (plain-file "iptables.rules" "*filter
+:INPUT DROP
+:FORWARD DROP
+:OUTPUT ACCEPT
+-A INPUT -i lo -j ACCEPT
+-A OUTPUT -o lo -j ACCEPT
+-A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+-A INPUT -j REJECT --reject-with icmp-port-unreachable
+COMMIT
+"))
+               (ipv6-rules (plain-file "ip6tables.rules" "*filter
+:INPUT DROP
+:FORWARD DROP
+:OUTPUT ACCEPT
+-A INPUT -i lo -j ACCEPT
+-A OUTPUT -o lo -j ACCEPT
+-A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+-A INPUT -j REJECT --reject-with icmp6-port-unreachable
+COMMIT
+"))))
+           (service login-service-type
+		    (login-configuration
+		      (motd (plain-file "motd" "\
+Ciao dal tuo sistema operativo!!!\n\n"))))
+	   (service slim-service-type
+		    (slim-configuration
+		      (xorg-configuration
+			(xorg-configuration (keyboard-layout keyboard-layout)))
+		      (auto-login? #t)
+		      (default-user "anonymous")))
+	   (modify-services %desktop-services
+			    (sysctl-service-type config =>
+						 (sysctl-configuration
+						   (settings (append '(("vm.swappiness" . "10"))
+								     %default-sysctl-settings))))
+			    (delete gdm-service-type))))
 
-           ;; This is the default list of services we
-           ;; are appending to.
-           %base-services))
   (bootloader (bootloader-configuration
                 (bootloader grub-bootloader)
                 (targets (list "/dev/sda"))
                 (keyboard-layout keyboard-layout)))
+
+  (kernel-arguments (list "l1tf=full,force mds=full,nosmt mitigations=auto,nosmt nosmt=force modprobe.blacklist=usbmouse,usbkbd quiet loglevel=3"))
+
   (swap-devices (list (swap-space
                         (target (uuid
-                                 "c5dc47aa-52db-4f70-b2d9-092ca089ef70")))))
+                                 "ebba7404-6956-41dc-9003-c02967e4eee7")))))
+  (mapped-devices (list (mapped-device
+                          (source (uuid
+                                   "b98f99e1-0760-458d-a7e7-6a6c01f562e9"))
+                          (target "home")
+                          (type luks-device-mapping))))
 
   ;; The list of file systems that get "mounted".  The unique
   ;; file system identifiers there ("UUIDs") can be obtained
   ;; by running 'blkid' in a terminal.
   (file-systems (cons* (file-system
+                         (mount-point "/home")
+                         (device "/dev/mapper/home")
+                         (type "ext4")
+			 (flags '(no-atime no-suid no-dev))
+                         (dependencies mapped-devices))
+                       (file-system
                          (mount-point "/")
                          (device (uuid
-                                  "ed590535-4048-406b-b287-d3096ed3d41d"
+                                  "81efaef5-e3ed-43b2-863b-0fe463b9a175"
                                   'ext4))
-                         (type "ext4"))
-                       (file-system
-                         (mount-point "/home")
-                         (device (uuid
-                                  "9655edd6-a71f-4e35-8ce0-f2ffd76f705a"
-                                  'ext4))
-                         (type "ext4")) %base-file-systems)))
+                         (type "ext4")
+			 (flags '(no-atime)))
+		       %base-file-systems)))
